0520f25563b41ae1815418a668766a2418247eab
“Cherylshire” <hsuanchang2017@gmail.com>
2020-01-10 19:11:21 -0600

    edit login page

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -11,11 +11,11 @@
           <li class="text-danger" v-for="error in errors">{{ error }}</li>
         </ul>
         <div class="form-group">
-          <label>Email:</label>
+          <label>Put Email Here:</label>
           <input type="email" class="form-control" v-model="email">
         </div>
         <div class="form-group">
-          <label>Password:</label>
+          <label>Put Password Here:</label>
           <input type="password" class="form-control" v-model="password">
         </div>
         <input type="submit" class="btn btn-primary" value="Submit">
